3fe6b4b4eef878ac7c8a5c99cf591b2cb35715c8
AchalS-iglu <iithebeastx666@gmail.com>
2021-10-23 15:29:19 +0530

    x close dashboard added and newdb turned to embeds

--- cogs/dashboard.py
+++ cogs/dashboard.py
@@ -26,8 +26,9 @@ class Dashboard(commands.Cog, name='Dashboard'):
 
         if ctx.author.id == 893202343913467905:
             return
-        
-        await ctx.message.reply("Enter a name for your new dashboard")
+
+        embed1 = discord.Embed(title='Create a Dashboard!', description="Enter a name for your new dashboard", color = (self.random_color))
+        embed1m = await ctx.message.reply(embed=embed1)
 
         while len(name) > 32:
             reply = await self.bot.wait_for('message')
@@ -40,15 +41,18 @@ class Dashboard(commands.Cog, name='Dashboard'):
             if len(name) > 32:
                 await reply.reply('Too long')
                 continue
-
-        await reply.reply('Now we will define playlists links / music links coupled with the theme name for {} dashboard'.format(name))
+        
+        #await embed1m.delete()
+        embed2 = discord.Embed(title='Create a Dashboard!', description='Now we will define playlists links / music links / names coupled with the theme name for {} dashboard'.format(name), color = self.random_color)
+        embed2m = await reply.reply(embed=embed2)
             
         finished = False
         count = 1
 
 
         while finished == False:
-            await reply.reply('Define the name of your {} theme!, reply with "finished" if you do not want any more themes. (MAX THEMES = 9)'.format((ordinaltg(count))))
+            embed3 = discord.Embed(title='Create a Theme!', description='Define the name of your {} theme!, reply with "finished" if you do not want any more themes. (MAX THEMES = 9)'.format((ordinaltg(count))), color = self.random_color)
+            embed3m = await reply.reply(embed=embed3)
 
             themename = '12345678901234567890123456789012345678901234567890123456789012345678901234567890'
 
@@ -76,7 +80,8 @@ class Dashboard(commands.Cog, name='Dashboard'):
 
             urls = []
 
-            await reply.reply('Now send all the links or YouTube titles you want to associate with this theme. One per message. Type "Done" when finished.')
+            embed4 = discord.Embed(title='Define your Theme!', description='Now send all the links or YouTube titles you want to associate with this theme. One per message. Type "Done" when all the names have been entered..', color = self.random_color)
+            embed4m = await reply.reply(embed=embed4)
 
             Done = False
             
@@ -132,6 +137,7 @@ class Dashboard(commands.Cog, name='Dashboard'):
         emojis = ('\U00000031\U0000fe0f\U000020e3', '\U00000032\U0000fe0f\U000020e3', '\U00000033\U0000fe0f\U000020e3', '\U00000034\U0000fe0f\U000020e3', '\U00000035\U0000fe0f\U000020e3', '\U00000036\U0000fe0f\U000020e3', '\U00000037\U0000fe0f\U000020e3', '\U00000038\U0000fe0f\U000020e3', '\U00000039\U0000fe0f\U000020e3')
         for i in range(0, len(themes)):
             await embedded.add_reaction(emojis[i])
+        await embedded.add_reaction('\U0000274e')
         
         def check(reaction, user):
             return user == ctx.message.author
@@ -142,16 +148,23 @@ class Dashboard(commands.Cog, name='Dashboard'):
 
         while bleh:
             reaction, user = await self.bot.wait_for('reaction_add', check=check)
-            if reaction != None:
+            reactionstr = ( emoji.demojize(reaction.emoji) )
+            if 'keycap' in reactionstr:
                 musicplayer.queue.empty()
                 await musicplayer.stop()
-                n = ( emoji.demojize(reaction.emoji) )[-2]
+                n = reactionstr[-2]
                 n = int(n) - 1
                 theme_to_play = ( list(themes)[n] )[-1]       
                 for x in theme_to_play:
                     await musiccog.play_command(ctx, query=x)
                 musicplayer.queue.shuffle()
-                await reaction.remove(user)               
+                await reaction.remove(user)     
+            else:
+                musicplayer.queue.empty()
+                await musicplayer.stop()
+                await musicplayer.teardown()
+                await embedded.delete()
+                break          
 
 
 
